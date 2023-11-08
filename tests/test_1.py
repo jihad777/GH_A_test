928fb1c893f5f54f99bd53ccbe0d4a8313547a1f
jihad777 <jihadbekkali1@gmail.com>
2023-11-08 16:04:19 +0100

    last

--- tests/test_1.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-sys.path.insert(0, "C:/Users/jbekkali/WORK/personal/github_actions/GH_A_test")
-
-from src.code_1 import reduced_price
-
-def test_reduced_price():
-    assert reduced_price(100, 0.50) == 50
