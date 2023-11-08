d3a69829310c43cb51f1faa7ca24fe6d85ffc6e0
jihad777 <jihadbekkali1@gmail.com>
2023-11-08 12:16:42 +0100

    last3

--- Tests/test_1.py
+++ Tests/test_1.py
@@ -1,9 +1,9 @@
-import sys
+#import sys
 import pytest
 
-sys.path.insert(0, '/Users/jbekkali/WORK/personal/github_actions/GH_A_test')
+#sys.path.insert(0, '/Users/jbekkali/WORK/personal/github_actions/GH_A_test')
 
-from code_1 import reduced_price
+#from code_1 import reduced_price
 
 @pytest.fixture
 def Test_reduced_price():
--- code_1.py
+++ code_1.py
@@ -1,3 +1,11 @@
+import pytest
+
 def reduced_price(amount, reduction):
     final_price = amount - amount*reduction
     return final_price
+    
+@pytest
+def Test_reduced_price():
+    price = 100
+    reduction = 50
+    assert reduced_price(price, reduction) == 50
