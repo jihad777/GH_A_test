fea05482e7fd0f993ea32bc52d1ae9f925e1ec01
jihad777 <jihadbekkali1@gmail.com>
2023-11-08 15:47:45 +0100

    new 2

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,4 +1,3 @@
-import pytest
 import sys
 
 sys.path.insert(0, "C:/Users/jbekkali/WORK/personal/github_actions/GH_A_test")
@@ -7,3 +6,6 @@ from src.code_1 import reduced_price
 
 def test_reduced_price():
     assert reduced_price(100, 0.50) == 50
+    assert reduced_price(80, 0.50) == 40
+    assert reduced_price(50, 0.50) == 25
+    assert reduced_price(10, 0.50) == 5
