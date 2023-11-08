52e0617636c085323dfe1a312f9f37a5ba545f09
jihad777 <jihadbekkali1@gmail.com>
2023-11-08 13:52:49 +0100

    last

--- test_1.py
+++
@@ -1,8 +0,0 @@
-from code_1 import reduced_price
-import pytest
-
-@pytest.fixture
-def Test_reduced_price():
-    price = 100
-    reduction = 0.50
-    assert reduced_price(price, reduction) == 50
