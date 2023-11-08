f3b7f175bbdf3d7b9ee7b6f8331a175f360fba0a
jihad777 <jihadbekkali1@gmail.com>
2023-11-08 15:59:35 +0100

    aaaaa

--- tests/test_1.py
+++ tests/test_1.py
@@ -6,6 +6,3 @@ from src.code_1 import reduced_price
 
 def test_reduced_price():
     assert reduced_price(100, 0.50) == 50
-    assert reduced_price(80, 0.50) == 40
-    assert reduced_price(50, 0.50) == 25
-    assert reduced_price(10, 0.50) == 5
