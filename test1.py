c3473d14f3cf3e4db1411f613aaa727044ce5e9f
jihad777 <jihadbekkali1@gmail.com>
2023-11-08 11:13:40 +0100

    test1 with test function

--- test1.py
+++ test1.py
@@ -1,5 +1,10 @@
+import pytest
+
 def reduced_price(amount, reduction):
     final_price = amount - amount*reduction
+    return final_price
+
+def test_reduced_price():
     assert reduction > 0
     assert reduction <= 1
-    return final_price
+    assert reduced_price(100, 0.50) == 50
