3ab7714995cfe711c41ad568c2992c8bb89a5f09
jihad777 <jihadbekkali1@gmail.com>
2023-11-08 16:54:06 +0100

    update

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,9 +1,11 @@
 import sys
 import pytest
 
-sys.path.insert(0, "C:/Users/jbekkali/WORK/personal/github_actions/GH_A_test/src/")
+sys.path.insert(0, "C:/Users/jbekkali/WORK/personal/github_actions/GH_A_test")
 
 from src.code_1 import reduced_price
+
+print(reduced_price(100, 0.50))
 
 def test_reduced_price():
     assert reduced_price(100, 0.50) == 50
