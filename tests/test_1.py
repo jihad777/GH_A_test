4e7e9474f48931f6ba0c146e6b182098231dd9c4
jihad777 <jihadbekkali1@gmail.com>
2023-11-08 13:56:35 +0100

    l

--- tests/test_1.py
+++ tests/test_1.py
@@ -5,5 +5,6 @@ sys.path.insert(0, "/Users/jbekkali/WORK/personal/github_actions/GH_A_test")
 
 from code_1 import reduced_price
 
+@pytest.fixture
 def Test_reduced_price():
     assert reduced_price(100, 0.50) == 50
