890a2f745313092f98bf0534fefffc4922d7b22f
jihad777 <jihadbekkali1@gmail.com>
2023-11-08 14:32:59 +0100

    Revert "l"
    This reverts commit 0e7e47e1bc309ee4ac8703ce8ca50fcc2338f7e5.

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
 
-sys.path.insert(0, "/Users/jbekkali/WORK/personal/github_actions/GH_A_test")
+sys.path.insert(0, "/Users/jbekkali/WORK/personal/github_actions/GH_A_test>")
 
 from code_1 import reduced_price
 
