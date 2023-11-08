3c9cd4f5f102639288263b178aadcfbc3941db8a
jihad777 <jihadbekkali1@gmail.com>
2023-11-08 12:08:02 +0100

    last

--- Tests/test_1.py
+++ Tests/test_1.py
@@ -1,5 +1,10 @@
-#from code_1 import reduced_price
+import sys
 import pytest
+
+sys.path.insert(0, '/Users/jbekkali/WORK/personal/github_actions/GH_A_test')
+
+from code_1 import reduced_price
+
 @pytest.fixture
 def Test_reduced_price():
     price = 100
