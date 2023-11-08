592642d929088e1c31bfafe6ec5a09e746838882
jihad777 <jihadbekkali1@gmail.com>
2023-11-08 11:32:25 +0100

    new test fixture

--- test1.py
+++ test1.py
@@ -4,5 +4,6 @@ def reduced_price(amount, reduction):
     final_price = amount - amount*reduction
     return final_price
 
+@pytest.fixture
 def test_reduced_price():
     assert reduced_price(100, 0.50) == 50
