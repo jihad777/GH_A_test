3f58cddf5eeba47c2545a171ef37163561cbf598
jihad777 <jihadbekkali1@gmail.com>
2023-11-08 11:29:16 +0100

    new test

--- test1.py
+++ test1.py
@@ -5,6 +5,4 @@ def reduced_price(amount, reduction):
     return final_price
 
 def test_reduced_price():
-    assert reduction > 0
-    assert reduction <= 1
     assert reduced_price(100, 0.50) == 50
